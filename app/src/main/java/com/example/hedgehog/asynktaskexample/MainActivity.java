ef1970ee5796e6fa56d0e53831ec553edcca0d25
hedgehog <research162@gmail.com>
2016-09-02 00:20:11 +0300

    link, unlink methods

--- app/src/main/java/com/example/hedgehog/asynktaskexample/MainActivity.java
+++ app/src/main/java/com/example/hedgehog/asynktaskexample/MainActivity.java
@@ -2,15 +2,15 @@ package com.example.hedgehog.asynktaskexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 import android.os.AsyncTask;
 
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-    static TextView textView;
+    TextView textView;
     MyTask mt;
+    static int currentCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +18,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.textView);
+        textView.setText("i = " + currentCount);
         mt = (MyTask) getLastCustomNonConfigurationInstance();
         if (mt == null) {
             mt = new MyTask();
             mt.execute();
         }
+        mt.link(this);
     }
 
     @Override
     public Object onRetainCustomNonConfigurationInstance() {
+        mt.unLink();
         return mt;
     }
 
     class MyTask extends AsyncTask<String, Integer, Void> {
+
+        MainActivity activity;
+        void link(MainActivity act) {
+            activity = act;
+        }
+
+        void unLink() {
+            activity = null;
+        }
 
         @Override
         protected Void doInBackground(String... params) {
@@ -38,9 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 for (int i = 1; i <= 30; i++) {
                     TimeUnit.SECONDS.sleep(1);
                     publishProgress(i);
-                    Log.d("qwe", "i = " + i
-                            + ", MyTask: " + this.hashCode()
-                            + ", MainActivity: " + MainActivity.this.hashCode());
+                    MainActivity.currentCount = i;
                 }
 
             } catch (InterruptedException e) {
@@ -53,7 +63,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
-            textView.setText("i = " + values[0]);
+            if (activity != null) {
+                activity.textView.setText("i = " + values[0]);
+            }
         }
     }
 }
